2fc2022a1ad6ba6e80af783d40996131aadaddd5
Rafi1702 <rafirizqi56@gmail.com>
2026-09-11 22:38:06 +0700

    update: add utils, component for food recommendation card

--- app/src/main/java/com/example/nutritiontracker/ui/home/page/HomePage.kt
+++ app/src/main/java/com/example/nutritiontracker/ui/home/page/HomePage.kt
@@ -4,42 +4,53 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Fastfood
+import androidx.compose.material.icons.filled.AddCircleOutline
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.nutritiontracker.domain.MacroType
+import coil3.compose.SubcomposeAsyncImage
+import com.example.nutritiontracker.datasource.remote.MOCK_FOODS
+import com.example.nutritiontracker.domain.Food
+import com.example.nutritiontracker.ui.home.components.MacroNutrientsNeed
 import com.example.nutritiontracker.ui.theme.colorScheme
 import com.example.nutritiontracker.ui.theme.typography
+import com.example.nutritiontracker.ui.utils.Debounce
+import com.example.nutritiontracker.ui.utils.withStyle
+import kotlinx.coroutines.delay
+import kotlin.math.roundToInt
 
 
 @Composable
-fun HomePage(modifier: Modifier) {
+fun HomePage(modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
         item {
             UserNeedSection()
@@ -47,107 +58,163 @@ fun HomePage(modifier: Modifier) {
     }
 }
 
-@Preview
+
+private suspend fun getFoods(): List<Food> {
+    delay(300)
+    return MOCK_FOODS
+}
+
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF808080
+)
+
+
 @Composable
-private fun UserNeedSection(modifier: Modifier = Modifier) {
-    Column{
-        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            MacroNutrientsNeed(
-                label = "PROTEIN",
-                current = 90f,
-                target = 130f,
-                unit = "g"
-            )
+private fun RecommendationFoodSection() {
+    var recommendationFoods by remember { mutableStateOf(MOCK_FOODS) }
+    Debounce("User Needs") {
+        getFoods()
+    }
 
-            MacroNutrientsNeed(
-                label = "PROTEIN",
-                current = 90f,
-                target = 130f,
-                unit = "g"
-            )
-
-            MacroNutrientsNeed(
-                label = "PROTEIN",
-                current = 90f,
-                target = 130f,
-                unit = "g"
-            )
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(recommendationFoods) { food ->
+            FoodCard(food = food)
         }
     }
 }
 
-
-
 @Composable
-private fun MacroNutrientsNeed(label: String, current: Float, target: Float, unit: String) {
-    Card(
-        modifier = Modifier
-            .width(120.dp)
-            .clip(RoundedCornerShape(8.dp)),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            modifier = Modifier.height(IntrinsicSize.Min),
+internal fun FoodCard(food: Food) {
+    val (_, name, macros) = food
+    Card(modifier = Modifier.clip(RoundedCornerShape(8.dp))) {
+        Column(
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            Box(
+            Row(
                 modifier = Modifier
-                    .width(8.dp)
-                    .background(colorScheme.primary)
-                    .fillMaxHeight()
-            )
-            Column(
-                modifier = Modifier.padding(vertical = 12.dp, horizontal = 12.dp),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
             ) {
+                SubcomposeAsyncImage(
+                    model = name,
+                    contentDescription = name,
+                    modifier = Modifier
+                        .size(96.dp),
+                    contentScale = ContentScale.Crop,
+                    loading = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(color = MaterialTheme.colorScheme.primary)
+                        )
+                    },
+                    error = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(
+                                    color = colorScheme.onTertiary,
+                                    shape = RoundedCornerShape(8.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "Error")
+                        }
+                    }
+                )
+                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                    Chip(radius = 16.dp, label = "Status")
+                    Text(text = "Nom NOm Nom Nom Nom Nom Nom Nom", style = typography.titleMedium)
+                    Text(text = buildAnnotatedString {
+                        withStyle(
+                            style = typography.labelMedium,
+                            color = LocalContentColor.current
+                        ) {
+                            append("345 kkal")
+                        }
+                    })
+
+                    FlowRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                        macros.forEach {
+                            val (type, serve) = it
+                            Chip(label = "${serve.roundToInt()}g ${stringResource(type.label)}")
+                        }
+                    }
+                }
+            }
+            FilledTonalButton(onClick = {}) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(
+                        8.dp,
+                        Alignment.CenterHorizontally
+                    ),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(label)
-                    Icon(Icons.Default.Fastfood, "${label}-icon", modifier = Modifier.size(16.dp))
+                    Icon(Icons.Default.AddCircleOutline, contentDescription = "add")
+                    Text("Tambah ke catatan")
                 }
-                Text(text = buildAnnotatedString {
-                    withStyle(
-                        style = typography.headlineSmall,
-                        color = colorScheme.onSurface
-                    ) {
-                        append(current.toInt().toString())
-                    }
-
-                    withStyle(
-                        style = typography.labelSmall,
-                        color = colorScheme.onSurface.copy(.5f)
-                    ) {
-                        append("/${target.toInt()}$unit")
-                    }
-                })
-                Text("Sisa ${(target - current).toInt()}$unit")
-                LinearProgressIndicator(
-                    progress = { current / target },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(8.dp)
-                )
             }
         }
     }
 }
 
-
-fun AnnotatedString.Builder.withStyle(
-    style: TextStyle,
-    color: Color,
-    block: AnnotatedString.Builder.() -> Unit,
-) {
-    withStyle(
-        style = SpanStyle(
-            color = color,
-            fontSize = style.fontSize,
-            fontWeight = style.fontWeight,
-            fontStyle = style.fontStyle,
-            fontFamily = style.fontFamily,
-            letterSpacing = style.letterSpacing,
-        ), block = block
+@Composable
+internal fun Chip(radius: Dp? = null, content: @Composable () -> Unit) {
+    Surface(
+        shape = if (radius != null) RoundedCornerShape(radius) else AssistChipDefaults.shape,
+        color = colorScheme.primary,
+        contentColor = colorScheme.onPrimary,
+        content = content
     )
+}
+
+@Composable
+internal fun Chip(radius: Dp? = null, label: String) {
+    Chip(radius) {
+        Text(label, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp), style = typography.labelSmall)
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF808080
+)
+@Composable
+private fun UserNeedSection(modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Text("Makro Nutrisi Hari Ini", style = typography.labelLarge)
+            Text(
+                "Target gram",
+                style = typography.labelSmall.copy(color = LocalContentColor.current.copy(alpha = .5f))
+            )
+        }
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            MacroNutrientsNeed(
+                label = "PROTEIN",
+                current = 90f,
+                target = 130f,
+                unit = "g"
+            )
+            MacroNutrientsNeed(
+                label = "PROTEIN",
+                current = 90f,
+                target = 130f,
+                unit = "g"
+            )
+            MacroNutrientsNeed(
+                label = "PROTEIN",
+                current = 90f,
+                target = 130f,
+                unit = "g"
+            )
+        }
+    }
 }
