118bd7e17766239ac1ebff03ea0ed50171789780
Rafi1702 <rafirizqi56@gmail.com>
2026-09-26 18:03:41 +0700

    update: simplify screen width condition for foldable screen size

--- app/src/main/java/com/example/nutritiontracker/ui/theme/ScreenSize.kt
+++ app/src/main/java/com/example/nutritiontracker/ui/theme/ScreenSize.kt
@@ -19,24 +19,14 @@ enum class ScreenSize {
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 internal fun rememberScreenSize(): ScreenSize {
-//    val activity = LocalActivity.current ?: return ScreenSize.MOBILE
-//
-//    val windowSizeClass = calculateWindowSizeClass(activity)
-
     val configuration = LocalConfiguration.current
     val screenWidthDp = configuration.screenWidthDp
 
     val customSizeClass = when {
         screenWidthDp < 600 -> ScreenSize.MOBILE
-        screenWidthDp < 840 || screenWidthDp < 1200 -> ScreenSize.FOLDABLE
+        screenWidthDp < 1200 -> ScreenSize.FOLDABLE
         screenWidthDp < 1600 -> ScreenSize.TABLET
         else -> ScreenSize.MOBILE
     }
-//    return when (windowSizeClass.widthSizeClass){
-//        WindowWidthSizeClass.Compact -> ScreenSize.MOBILE
-//        WindowWidthSizeClass.Medium -> ScreenSize.FOLDABLE
-//        WindowWidthSizeClass.Expanded, WindowWidthSizeClass.Large -> ScreenSize.FOLDABLE
-//        else -> ScreenSize.MOBILE
-//    }
     return customSizeClass
 }
